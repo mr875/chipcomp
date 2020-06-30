f110c6bc52bb708e1baf806d4ba7d791fde3313b
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-06-30 11:55:15 +0100

    minor upd

--- run_connect.py
+++ run_connect.py
@@ -12,5 +12,11 @@ chipcursor=chip.getCursor()
 chipcursor.execute(query2)
 for row in chipcursor.fetchall():
     print(row)
+query3 = ('select * from dil_frq limit 1')
+brcursor.execute(query3)
+for row in brcursor.fetchall():
+    print(row)
 br.close()
 chip.close()
+#todo: cursor should probably be closed after use and can then open a new one. should move this behaviour to DBConnect
+#https://stackoverflow.com/questions/5504340/python-mysqldb-connection-close-vs-cursor-close
