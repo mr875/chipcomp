1975b94e225af3a6c64115c36ba913734fe49e51
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-07-21 16:33:48 +0100

    added child class InfImmun24v2(InfCorEx24v1_1a1) for parsing InfiniumImmunoArray_annotation.csv

--- chipreader.py
+++ chipreader.py
@@ -216,8 +216,8 @@ class InfCorEx24v1a1(ChipReader):
     # test excerpt: /mnt/HPC/processed/mr875/tasks/dsp367/corev1_0_rsEg.csv
     # location: /mnt/HPC/processed/Metadata/variant_annotation/CoreExomev1.0_annotation.csv
 
-    def __init__(self,fname):
-        super().__init__(fname)
+    def __init__(self,fname,delim=','):
+        super().__init__(fname,delim)
         self.GRCh37='37'
         self.datasource = "170"
 
@@ -239,14 +239,20 @@ class InfCorEx24v1a1(ChipReader):
 class InfCorEx24v1_1a1(InfCorEx24v1a1):
     # test excerpt: /mnt/HPC/processed/mr875/tasks/dsp367/corev1_1_rsEg.csv
     # location: /mnt/HPC/processed/Metadata/variant_annotation/CoreExomev1.1_annotation.csv
-    def __init__(self,fname):
-        super().__init__(fname)
+    def __init__(self,fname,delim=','):
+        super().__init__(fname,delim)
         self.datasource = "171"
 
     def load_custom(self):
         self.flankseqcols=["Forward_Seq","Design_Seq","Top_Seq"]
         self.flankseqcoln = self.fillcust(self.flankseqcols)
         
+class InfImmun24v2(InfCorEx24v1_1a1):
+    # test excerpt: /mnt/HPC/processed/mr875/tasks/dsp367/infimmun_Eg.csv
+    # location: /mnt/HPC/processed/Metadata/variant_annotation/InfiniumImmunoArray_annotation.csv
+    def __init__(self,fname):
+        super().__init__(fname,"\t")
+        self.datasource = "70"
 
 class InfEx24v1a2(ChipReader):
     # test excerpt: /mnt/HPC/processed/mr875/tasks/dsp367/InfiniumExome-24v1-0_A2_Eg.csv
--- run_reader.py
+++ run_reader.py
@@ -7,11 +7,12 @@ from chipreader import *
 readers = [InfCorEx24v1a1('/mnt/HPC/processed/mr875/tasks/dsp367/corev1_0_rsEg.csv'),
     InfEx24v1a2('/mnt/HPC/processed/mr875/tasks/dsp367/InfiniumExome-24v1-0_A2_Eg.csv'),
     Dil('/mnt/HPC/processed/mr875/tasks/dsp367/DIL_annotation_Eg.csv'),
-    AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/AxiUKBBAffy2_1_38_Eg.csv')]
+    AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/AxiUKBBAffy2_1_38_Eg.csv'),
+    InfImmun24v2('/mnt/HPC/processed/mr875/tasks/dsp367/infimmun_Eg.csv')]
 
 for reader in readers:
 #    print(reader.header)
-#    if not type(reader).__name__ == "AxiUKBBAffy2_1":
+#    if not type(reader).__name__ == "InfImmun24v2":
 #        continue
     for l in reader.linebyline(3):
         line_dict = reader.proc_line(l)
