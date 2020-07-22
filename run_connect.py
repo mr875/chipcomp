0f3abba3a37249ae211bcdaea91993d5d0644fa3
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-07-22 17:38:25 +0100

    added some error catching. one error about sql connection i can not solve, so am trying to parse latest file (Axiom_UKBBv2_1.na36.r1.a1.annot.csv) into a fresh db in case the the old one is corrupted

--- run_connect.py
+++ run_connect.py
@@ -1,3 +1,4 @@
+import mysql.connector
 import time
 import datetime
 import logging
@@ -19,10 +20,11 @@ def readin(chip,reader):
     if reader.col_GRCh37_pos:
         build = "37"
     new_ds = reader.datasource
+    prev_id = "first"
     for line in reader.linebyline():
         if logline == variant_count:
             now = int(time.time() - start)
-            logging.info("approximately %.2f%% parsed after %s seconds, %s variants" % ((variant_count/line_count*100),now,variant_count))
+            logging.info("approximately %.2f%% parsed after %s seconds, %s variants" % ((variant_count/line_count*100),now,variant_count-1))
             logline += fvper
         try:
             dic = reader.proc_line(line)
@@ -30,13 +32,25 @@ def readin(chip,reader):
             statement = "IndexError when trying to process line (reader.proc_line).\nProbably this line doesn't have all expected columns:\n"+ "--".join(line) + "\nskipping this line"
             logging.warning(statement)
             continue
+        except ValueError as ve:
+            statement = "ValueError thrown (reader.proc_line).\nprobably from int conversion of ukbb style '---' reference positions. Last variation parsed was {}. Error: {}\nskipping this variant".format(prev_id,ve)
+            logging.warning(statement)
+            continue
+        else:
+            prev_id = dic['uid']
         try:
             variant = VariantI(curs,dic,new_ds,build)
             variant.log_flank()
             variant.log_probe()
             variant.log_coord()
+        except mysql.connector.errors.DataError as de:
+            mess = "error thrown, suspected long chromosome name ({}). skipping {}/{}: {}".format(dic['chr'],dic['uid'],dic['snp_id'],de)
+            logging.warning(mess)
+            chip.rollback()
         except Exception as e:
-            print("error with %s/%s" % (dic['uid'],dic['snp_id']))
+            mess = "error with {}/{}".format(dic['uid'],dic['snp_id'])
+            logging.error(mess)
+            print(mess)
             chip.rollback()
             raise
         else:
@@ -49,17 +63,23 @@ def readin(chip,reader):
 #        InfEx24v1a2('/mnt/HPC/processed/mr875/tasks/dsp367/InfiniumExome-24v1-0_A2_Eg.csv'),
 #        Dil('/mnt/HPC/processed/mr875/tasks/dsp367/DIL_annotation_Eg.csv'),
 #       InfCorEx24v1_1a1('/mnt/HPC/processed/mr875/tasks/dsp367/corev1_1_rsEg.csv'),
-#           AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/AxiUKBBAffy2_1_38_Eg.csv')]
+#           AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/AxiUKBBAffy2_1_38_Eg.csv'),
+        #InfImmun24v2('/mnt/HPC/processed/mr875/tasks/dsp367/infimmun_Eg.csv')]
+# debug:
+#readers = [AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/AxiUKBBAffy2_1_38_Eg.csv')]
+
 
 #readers = [InfCorEx24v1a1('/mnt/HPC/processed/Metadata/variant_annotation/CoreExomev1.0_annotation.csv')]
 #readers = [InfEx24v1a2('/mnt/HPC/processed/Metadata/variant_annotation_grch38/InfiniumExome-24v1-0_A2.csv')]
-readers = [InfEx24v1a2('/mnt/HPC/processed/mr875/tasks/dsp367/InfiniumExome-24v1-0_A2_Eg.csv')]
-#readers = [Dil('/mnt/HPC/processed/Metadata/variant_annotation/DIL_annotation.csv')]
 #readers = [InfCorEx24v1_1a1('/mnt/HPC/processed/Metadata/variant_annotation/CoreExomev1.1_annotation.csv')]
-#readers = [AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/Axiom_UKBBv2_1.na36.r1.a1.annot.csv')]
+
+readers = [AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/Axiom_UKBBv2_1.na36.r1.a1.annot.csv')]
+#readers = [Dil('/mnt/HPC/processed/Metadata/variant_annotation/DIL_annotation.csv')]
+#readers = [InfImmun24v2('/mnt/HPC/processed/Metadata/variant_annotation/InfiniumImmunoArray_annotation.csv')]
 
 
-ch = DBConnect("chip_comp")
+ch = DBConnect("cc2")
+#ch = DBConnect("chip_comp")
 logfile = datetime.datetime.now().strftime("%a_%d%b_%I%p.log")
 logging.basicConfig(filename=logfile, level=logging.INFO)
 for source in readers:
