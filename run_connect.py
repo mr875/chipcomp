cc41e3a59899317a986e78d4c76a6bbc6e53191b
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-07-24 12:33:03 +0100

    added offset capacity so that, for one source file, it does not need to start at 'line 1' (the first variant under the header) but can start from any. This should help for long run times if the program exited before the file was fully parsed

--- run_connect.py
+++ run_connect.py
@@ -7,10 +7,10 @@ from varianti import VariantI
 from chipreader import *
 from connect import DBConnect
 
-def readin(chip,reader):
+def readin(chip,reader,offset=0):
     orig_file = reader.fname
     line_count = int(check_output(["wc", "-l", orig_file]).split()[0])
-    variant_count = 1
+    variant_count = 0
     start = time.time()
     logging.info('file: %s',(orig_file))
     fvper = int(0.05 * line_count)
@@ -22,10 +22,13 @@ def readin(chip,reader):
     new_ds = reader.datasource
     prev_id = "first"
     for line in reader.linebyline():
+        variant_count+=1
         if logline == variant_count:
             now = int(time.time() - start)
             logging.info("approximately %.2f%% parsed after %s seconds, %s variants" % ((variant_count/line_count*100),now,variant_count-1))
             logline += fvper
+        if variant_count < offset:
+            continue
         try:
             dic = reader.proc_line(line)
         except IndexError:
@@ -41,7 +44,10 @@ def readin(chip,reader):
             ch.close()
             raise
         else:
-            prev_id = dic['uid']
+            if 'uid' in dic:
+                prev_id = dic['uid']
+            else:
+                prev_id = dic['snp_id']
         try:
             variant = VariantI(curs,dic,new_ds,build)
             variant.log_flank()
@@ -60,23 +66,22 @@ def readin(chip,reader):
             raise
         else:
             chip.commit()
-            variant_count += 1
     now = int(time.time() - start)
-    logging.info('Finished: %s seconds passed, %s variants' % (now,(variant_count-1)))
+    logging.info('Finished: %s seconds passed, %s variants' % (now,(variant_count)))
 
-readers = [InfCorEx24v1a1('/mnt/HPC/processed/mr875/tasks/dsp367/corev1_0_rsEg.csv'),
-        InfEx24v1a2('/mnt/HPC/processed/mr875/tasks/dsp367/InfiniumExome-24v1-0_A2_Eg.csv'),
-        Dil('/mnt/HPC/processed/mr875/tasks/dsp367/DIL_annotation_Eg.csv'),
-       InfCorEx24v1_1a1('/mnt/HPC/processed/mr875/tasks/dsp367/corev1_1_rsEg.csv'),
-           AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/AxiUKBBAffy2_1_38_Eg.csv'),
-        InfImmun24v2('/mnt/HPC/processed/mr875/tasks/dsp367/infimmun_Eg.csv')]
+#readers = [InfCorEx24v1a1('/mnt/HPC/processed/mr875/tasks/dsp367/corev1_0_rsEg.csv'),
+#        InfEx24v1a2('/mnt/HPC/processed/mr875/tasks/dsp367/InfiniumExome-24v1-0_A2_Eg.csv'),
+#        Dil('/mnt/HPC/processed/mr875/tasks/dsp367/DIL_annotation_Eg.csv'),
+#       InfCorEx24v1_1a1('/mnt/HPC/processed/mr875/tasks/dsp367/corev1_1_rsEg.csv'),
+#           AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/AxiUKBBAffy2_1_38_Eg.csv'),
+#        InfImmun24v2('/mnt/HPC/processed/mr875/tasks/dsp367/infimmun_Eg.csv')]
 # debug:
 #readers = [AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/AxiUKBBAffy2_1_38_Eg.csv')]
 
 
-#readers = [InfCorEx24v1a1('/mnt/HPC/processed/Metadata/variant_annotation/CoreExomev1.0_annotation.csv')]
-#readers = [InfEx24v1a2('/mnt/HPC/processed/Metadata/variant_annotation_grch38/InfiniumExome-24v1-0_A2.csv')]
-#readers = [InfCorEx24v1_1a1('/mnt/HPC/processed/Metadata/variant_annotation/CoreExomev1.1_annotation.csv')]
+readers = [InfCorEx24v1a1('/mnt/HPC/processed/Metadata/variant_annotation/CoreExomev1.0_annotation.csv'),
+        InfEx24v1a2('/mnt/HPC/processed/Metadata/variant_annotation_grch38/InfiniumExome-24v1-0_A2.csv'),
+        InfCorEx24v1_1a1('/mnt/HPC/processed/Metadata/variant_annotation/CoreExomev1.1_annotation.csv')]
 
 #readers = [AxiUKBBAffy2_1('/mnt/HPC/processed/mr875/tasks/dsp367/Axiom_UKBBv2_1.na36.r1.a1.annot.csv')]
 
@@ -88,8 +93,13 @@ ch = DBConnect("cc2")
 #ch = DBConnect("chip_comp")
 logfile = datetime.datetime.now().strftime("%a_%d%b_%I%p.log")
 logging.basicConfig(filename=logfile, level=logging.INFO)
+offsetclass = "" #"AxiUKBBAffy2_1"  #pick a source class from which you don't want to parse from the beginning
+offsetvariant = 3 # variant 1 = the first line under the header. so if offsetvariant = 3 then the 3rd variant will be parsed
 for source in readers:
-    readin(ch,source)
+    if type(source).__name__ == offsetclass:
+        readin(ch,source,offsetvariant)
+    else:
+        readin(ch,source)
 ch.close()
 
 
