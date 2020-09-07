c35ff4d9603decdd7ad7e96edee64f9fce732dcc
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-09-07 16:58:30 +0100

    started new stage: walk through sequential positions and check the variation is shifting too

---
+++ chromwalk.py
@@ -0,0 +1,36 @@
+from resolvef import ResolveF
+from connect import DBConnect
+from queryfile import QueryFile
+import time
+import datetime
+import logging
+
+
+def main():
+
+    db = 'chip_comp'
+    q = "SELECT DISTINCT chr FROM positions"
+    conn = DBConnect(db)
+    curs = conn.getCursor()
+    try:
+        curs.execute(q)
+        chrs = curs.fetchall()
+    except Exception as e:
+        print("closing")
+        conn.close()
+        raise
+
+    chrs = [st[0] for st in chrs]
+    for chrm in chrs[0]:
+        fname = chrm + ".txt"
+        q = "SELECT id,pos FROM positions WHERE chr = %s ORDER BY pos ASC LIMIT 100" # remove limit after testing
+        vals = (chrm,)
+        qf = QueryFile(fname,q,vals,db)
+
+
+    
+    
+    conn.close()
+
+if __name__ == '__main__':
+    main()
