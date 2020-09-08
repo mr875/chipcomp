ea9f67d72f39b5aa7285731f7e21cd30ba5034d4
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-09-08 16:28:06 +0100

    progress on comparing sequential variants

--- chromwalk.py
+++ chromwalk.py
@@ -5,8 +5,66 @@ import time
 import datetime
 import logging
 
-def walk(qfit,curs):
-    pass
+class WalkF(ResolveF):
+
+    def __init__(self):
+        self.switchdic = {"A":"T","C":"G","G":"C","T":"A"}
+        self.switchextra = {"N":"N","R":"Y","Y":"R","S":"S","W":"W","K":"M","M":"K","B":"V","V":"B","D":"H","H":"D"}
+
+    def threeprimes(self,flank,maxstep=2):
+        basen = maxstep + 3
+        left,rev = self.leftright(flank,basen)
+        orig = ']' + flank.split(']')[1][:basen] 
+        orig = orig.upper()
+        return [orig,rev]
+
+def walk(qfit,curs,maxstep=2):
+    walkf = WalkF()
+    vid,pos = next(qfit)
+    pos = int(pos)
+    increment_set_succ = set()
+    increment_set_fail = set()
+    old_diff = 0
+    for nxtv,nxtp in qfit:
+        nxtp = int(nxtp)
+        diff = nxtp - pos
+        if 0 < diff <= maxstep:
+            print("for difference of %s, found %s and %s" % (diff,pos,nxtp))
+            first = getflank(curs,vid)
+            second = getflank(curs,nxtv)
+            f3prime = walkf.threeprimes(first,maxstep)[0]
+            s3primes = walkf.threeprimes(second,maxstep)
+            s3primes = [bs[1:4] for bs in s3primes]
+            expsecond = f3prime[diff+1:diff+4] # +1 accounts for ']' and +4 makes the triplet 
+            if expsecond in s3primes:
+                increment_set_succ.add(vid)
+                increment_set_succ.add(nxtv)
+            else:
+                increment_set_fail.add(vid)
+                increment_set_fail.add(nxtv)
+            old_diff = diff
+        else: # reset sets
+            if increment_set_succ:
+                print("Successful increments (%s bps a time)" % (old_diff))
+                print(increment_set_succ)
+            if increment_set_fail:
+                print("unsuccessful increments (%s bps a time)" % (old_diff))
+                print(increment_set_fail)
+            increment_set_succ.clear()
+            increment_set_fail.clear()
+        vid = nxtv
+        pos = nxtp
+
+def compneigh(curs,pos1,id1,pos2,id2):
+    diff = pos2-pos1
+
+def getflank(curs,vid):
+    q = "SELECT flank_seq FROM flank WHERE id = %s"
+    vals = (vid,)
+    curs.execute(q,vals)
+    flanks = curs.fetchall()
+    flank = flanks[0][0] # take first one. later: may add ranking to flank seqs and then ordered output will be possible
+    return flank
 
 def itls(qf):
     for ls in qf.readls():
@@ -27,9 +85,9 @@ def main():
         raise
 
     chrs = [st[0] for st in chrs]
-    for chrm in chrs[0]:
+    for chrm in chrs[0]: #remove index[] after testing
         fname = chrm + ".txt"
-        q = "SELECT id,pos FROM positions WHERE chr = %s ORDER BY pos ASC LIMIT 100" # remove limit after testing
+        q = "SELECT id,pos FROM positions WHERE chr = %s ORDER BY pos ASC LIMIT 220" # remove limit after testing
         vals = (chrm,)
         qf = QueryFile(fname,q,vals,db)
         qfit = itls(qf)
