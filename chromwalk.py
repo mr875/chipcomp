6ca65d9d3508feeb9eaa2789bf219cf4b36d31a1
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-09-09 11:37:12 +0100

    added logging

--- chromwalk.py
+++ chromwalk.py
@@ -18,7 +18,13 @@ class WalkF(ResolveF):
         orig = orig.upper()
         return [orig,rev]
 
-def walk(qfit,curs,maxstep=2):
+def walk(qf,curs,maxstep=2):
+    rc = qf.row_count
+    fvper = int(0.20 * rc)
+    logline = fvper
+    start = time.time()
+    count = 0
+    qfit = itls(qf)
     walkf = WalkF()
     vid,pos = next(qfit)
     pos = int(pos)
@@ -54,9 +60,18 @@ def walk(qfit,curs,maxstep=2):
             increment_set_fail.clear()
         vid = nxtv
         pos = nxtp
+        count += 1
+        if logline == count:
+            now = int(time.time() - start)
+            logging.info("approximately %.2f%% parsed (of current chromosome) after %s seconds, %s positions/lines, line: %s,%s" % ((count/rc*100),now,count,vid,pos))
+            logline += fvper
+    if increment_set_succ:
+        print("successful increments (%s bps a time) at end of file" % (old_diff))
+        print(increment_set_succ)
+    if increment_set_fail:
+        print("unsuccessful increments (%s bps a time) at end of file" % (old_diff))
+        print(increment_set_fail)
 
-def compneigh(curs,pos1,id1,pos2,id2):
-    diff = pos2-pos1
 
 def getflank(curs,vid):
     q = "SELECT flank_seq FROM flank WHERE id = %s"
@@ -83,15 +98,25 @@ def main():
         print("closing")
         conn.close()
         raise
-
+    logfile = datetime.datetime.now().strftime("walk_%a_%d%b_%I%p.log")
+    logging.basicConfig(filename=logfile, level=logging.INFO)
+    start = time.time()
+    count = 0
     chrs = [st[0] for st in chrs]
+    logging.info('chromwalk.py: found %s chromosomes to walk: %s',len(chrs),','.join(chrs))
     for chrm in chrs[0]: #remove index[] after testing
-        fname = chrm + ".txt"
+        fname = "walk_" + chrm + ".txt"
         q = "SELECT id,pos FROM positions WHERE chr = %s ORDER BY pos ASC LIMIT 220" # remove limit after testing
         vals = (chrm,)
         qf = QueryFile(fname,q,vals,db)
+        count += qf.row_count
+        logging.info('starting %s, file has %s lines',fname,qf.row_count)
         qfit = itls(qf)
-        walk(qfit,curs) 
+        walk(qf,curs) 
+        now = int(time.time() - start)
+        logging.info('done %s, took %s seconds',fname,now)
+    now = int(time.time() - start)
+    logging.info('Finished all chromosomes after %s seconds (%s rows)' % (now,count))
     
     conn.close()
 
