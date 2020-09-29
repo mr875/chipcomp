c9588e1be58a37b9a053612acf7bef989206c8e4
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-09-29 12:07:45 +0100

    adding logging

--- getflank.py
+++ getflank.py
@@ -1,3 +1,7 @@
+import sys
+import time
+import datetime
+import logging
 from queryfile import NormFile
 import requests
 import urllib.request
@@ -13,7 +17,7 @@ def getdetails(rsid):
         req = requests.get(url)
         root = ET.fromstring(req.content)
     except Exception:
-        print("a problem occurred when trying to fetch (or load):",url)
+        logging.error("a problem occurred when trying to fetch (or load):",url)
     message = "trouble finding %s Element for rsid %s:\n" + url
     namesp = root.tag.split("}")[0] + "}" # eg: {https://www.ncbi.nlm.nih.gov/SNP/docsum}DocumentSummary
     tofind = namesp + 'DocumentSummary'
@@ -37,13 +41,15 @@ def getdetails(rsid):
     if allele_elm is not None:
         allele = allele_elm.text
     else:
-        raise XMLParseError(message % (tofind,rsid))
+        logging.info("%s: can't find %s\nbut not needed" % (rsid,tofind))
+        #raise XMLParseError(message % (tofind,rsid))
     tofind = namesp + 'GENES/' + namesp + 'GENE_E/' + namesp + 'NAME'
     genename_elm = main.find(tofind)
     if genename_elm is not None:
         genename = genename_elm.text
     else:
-        raise XMLParseError(message % (tofind,rsid))
+        logging.info("%s: can't find %s\nbut not needed" % (rsid,tofind))
+        #raise XMLParseError(message % (tofind,rsid))
     chrm = pos.split(':')[0]
     pos = pos.split(':')[1]
     return (chrm,pos,acc,allele,genename)
@@ -65,14 +71,25 @@ def getflank(pos,acc):
     return left,right 
 
 def main():
-    rsids = NormFile('rsids.txt')
-    exfl = open('external_flanks.txt','a+')
+    rsids = NormFile('exflank/rsids.txt')
+    batch = 500
+    rc = rsids.row_count
+    logfile = datetime.datetime.now().strftime("gfl_%a_%d%b_%I%p.log")
+    logging.basicConfig(filename=logfile, level=logging.INFO)
+    fvper = int(0.05 * batch)
+    logline = fvper
+    start = time.time()
     startid = ""
+    strt_mess = 'from beginning of file'
+    if startid:
+        strt_mess = "from line containing '" + startid + "'"
     count = 0
     fail_count = 0;
     max_fail = 10
-    batch = 500
+    logging.info('file %s has %s lines. Doing a batch of %s lines %s.' % (rsids.bfile,rc,batch,strt_mess))
+    exfl = open('exflank/external_flanks.txt','a+')
     cont = True
+    endof = True
     for line in rsids.readls():
         if line[0] == startid:
             cont = False
@@ -80,7 +97,14 @@ def main():
             continue
         count += 1
         if count > batch:
+            endof = False
+            now = int(time.time() - start)
+            logging.info('finished batch of %s. Next line: %s. Took %s seconds' % (batch,line[0],now))
             break
+        if logline == count:
+            now = int(time.time() - start)
+            logging.info("approximately %.2f%% of batch done. %s seconds. line %s" % ((count/batch*100),now,line[0]))
+            logline += fvper
         try:
             chrm,pos,acc,allele,genename = getdetails(line[0])
             left,right = getflank(pos,acc)
@@ -88,15 +112,16 @@ def main():
         except Exception as e:
             fail_count+=1
             statement = "error (%s) encountered at line/rs %s\n%s\n%s" % (fail_count,line[0],str(sys.exc_info()[0]),str(e))
-            print(statement) # swap for logging file
-            #logging.error(statement)
+            logging.error(statement)
             if fail_count >= max_fail:
                 exfl.close()
                 statement = "maximum error count reached, leaving process"
-                #logging.error(statement)
+                logging.error(statement)
                 raise 
     exfl.close()
-        
+    if endof:
+        now = int(time.time() - start)
+        logging.info('reached end of file %s after %s seconds' % (rsids.bfile,now))
 
 if __name__ == '__main__':
     main()
