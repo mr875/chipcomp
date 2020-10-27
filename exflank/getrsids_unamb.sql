fcd0043d091f31193c5a974a2e1b93ccf6fef0d1
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-10-27 15:30:53 +0000

    edited to filter out rsids that have already been throuhg external comparison process

--- exflank/getrsids_unamb.sql
+++ exflank/getrsids_unamb.sql
@@ -1,9 +1,13 @@
 # mysql chip_comp < exflank/getrsids_unamb.sql > exflank/rsids.txt
 SELECT id FROM flank
 WHERE id NOT IN
-    (
-    SELECT DISTINCT(id) from flank where flank_seq like '%N%' or flank_seq like '%K%' or flank_seq like '%M%' or flank_seq like '%R%' or flank_seq like '%Y%' or flank_seq like '%S%' or flank_seq like '%W%'
-    )
+        (
+        SELECT DISTINCT(id) from flank where flank_seq like '%N%' or flank_seq like '%K%' or flank_seq like '%M%' or flank_seq like '%R%' or flank_seq like '%Y%' or flank_seq like '%S%' or flank_seq like '%W%'
+        )
+AND id NOT IN
+        (
+        SELECT DISTINCT(id) FROM flank WHERE chosen = 1
+        ) 
 AND id like 'rs%'
 GROUP BY id 
 HAVING count(*) > 1
