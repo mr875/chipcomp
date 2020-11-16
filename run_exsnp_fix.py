b389f62b65d0bbb2acea2e4d436f1dad720ec357
Marc Rossello <mr875@mr875@bioresource.nihr.ac.uk>
2020-11-16 16:47:44 +0000

    further progress

--- run_exsnp_fix.py
+++ run_exsnp_fix.py
@@ -1,16 +1,17 @@
 import sys
 from queryfile import NormFile
 from connect import DBConnect
+from varianti import VariantM
 
 def buildrsmerge(curs,rsid,refpos,altid):
-    q = 'SELECT build,concat(chr,":",pos) FROM positions WHERE id = %s'
+    q = 'SELECT build,concat(chr,":",pos),datasource FROM positions WHERE id = %s'
     vals = (rsid,)
     curs.execute(q,vals)
     tsev = 0
     teight = 0
     dbrefpos38 = []
     dbrefpos37 = []
-    for build,chrpos in curs.fetchall():
+    for build,chrpos,ds in curs.fetchall():
         if build == '37':
             dbrefpos37.append(chrpos)
             tsev +=1
@@ -18,18 +19,37 @@ def buildrsmerge(curs,rsid,refpos,altid):
             dbrefpos38.append(chrpos)
             teight += 1
     if teight < 1 and tsev > 0:
-        print("rsid %s on build 37 can be merged with %s (build 38)" % (rsid,altid)) # but is altid present in build 37 as well? if so it will have different coords or it will have been merged at a previous stage
         q = q + ' AND build = %s'
         curs.execute(q,(altid,'37'))
         b37s = curs.fetchall() 
-        print(len(b37s))
+        if len(b37s) > 0:
+            print('rsid %s can not be merged with alt_id %s because alt_id has a build 37 position (%s) (would have been merged at earlier stage if they matched)' % (rsid,altid,len(b37s)))
+            return
+        print("attempting merging of rsid %s on build 37 with %s on build 38" % (rsid,altid)) 
+        curs.execute(q,(altid,'38'))
+        b38s = curs.fetchall()
+        # first insert rsid into build 38 so it can be merged with alt_id (that's in build 38)
+        ins = 'INSERT IGNORE INTO positions (id,chr,pos,build,datasource) VALUES (%s,%s,%s,%s,%s)'
+        curs.execute(ins,(rsid,refpos.split(':')[0],refpos.split(':')[1],'38','dbsnp'))
+        VariantM(curs,rsid,refpos.split(':')[1],'38','dbsnp',refpos.split(':')[0]).snpid_swapin(altid,b38s[0][2])
     if teight > 0 and '0:0' not in dbrefpos38:
         print("rsid %s exists in db with build 38 coords, can not merge with suggested %s" % (rsid,altid)) 
     if teight > 0 and '0:0' in dbrefpos38:
         print("rsid %s already exists under build 38 but with no coords (0:0)" % (rsid))
 
+
 def xyrsmerge(curs,rsid,refpos,altid):
-    print("entered xyrsmerge")
+    q = 'SELECT chr FROM positions WHERE id = %s'
+    val = (rsid,)
+    curs.execute(q,val)
+    chrs = curs.fetchall()
+    chrs = set([c[0] for c in chrs])
+    chrs.add(refpos.split(':')[0])
+    if len(chrs) == 1:
+        buildrsmerge(curs,rsid,refpos,altid)
+        return
+    print('adding %s as alt_id to %s because rsid exists already but with different X/Y/XY chr' % (rsid,altid))
+    VariantM(curs,None,None,None,None,None).add_altid(altid,rsid,'dbsnp')
 
 def main(argv):
     
@@ -41,14 +61,15 @@ def main(argv):
     conn = DBConnect("cc4")
     curs = conn.getCursor()
     for rsid,refpos,altid in infile.readls(dlim=" "):
-        if refpos.split(':')[0].isnumeric():
-            try:
+        try:
+            if refpos.split(':')[0].isnumeric():
                 buildrsmerge(curs,rsid,refpos,altid)
-            except:
-                conn.close()
-                raise
-        else:
-            xyrsmerge(curs,rsid,refpos,altid)
+            else:
+                xyrsmerge(curs,rsid,refpos,altid)
+            conn.commit()
+        except:
+            conn.close()
+            raise
     conn.close()
 
 if __name__ == '__main__':
