6d2867b5bfab2befbd136e6c864bec0018c8e25f
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2021-03-25 17:47:58 +0000

    new branch to parse new version of ukbb2.1 annot

--- run_connect.py
+++ run_connect.py
@@ -21,7 +21,7 @@ def readin(chip,reader,offset=0):
         build = "37"
     new_ds = reader.datasource
     prev_id = "first"
-    for line in reader.linebyline(): 
+    for line in reader.linebyline(2): # remove int when testing done
         variant_count+=1
         if logline == variant_count:
             now = int(time.time() - start)
